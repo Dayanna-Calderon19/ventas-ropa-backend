60b492b3671b70cb9bad7934ac48eaaf52d3cbcb
Dayanna-Calderon19 <aracellycalderonlozano28@gmail.com>
2026-06-25 17:28:59 -0500

    feat: actualizar esquema y configuración de base de datos

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
 
-const adapter = new PrismaPg({
+const { Pool } = pg;
+
+const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
 });
+
+const adapter = new PrismaPg(pool);
 
 const prisma = new PrismaClient({
     adapter,
